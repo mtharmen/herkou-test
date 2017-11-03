4d38682e975b7d011bc65d26e65d7ee3522ce031
mtharmen <m.tharmendran@gmail.com>
2017-11-03 19:15:07 -0400

    testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,47 +7,16 @@ import { HttpClientModule } from '@angular/common/http'
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 import { ApiService } from './core/api.service'
-import { AuthService } from './core/auth.service'
-import { ConfirmService } from './core/misc/confirm.service'
-import { LoginFormService } from './pages/login-form/login-form.service'
-import { ChartService } from './core/chart.service'
-
-import { SearchFilterPipe } from './core/search-filter.pipe'
 
 import { AppRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component'
-import { HeaderComponent } from './header/header.component'
-import { FooterComponent } from './footer/footer.component'
-import { LoadingComponent } from './core/misc/loading.component'
 import { HomeComponent } from './pages/home/home.component'
-import { ProfileComponent } from './pages/profile/profile.component'
-import { PollComponent } from './pages/poll/poll.component'
-import { AdminComponent } from './pages/admin/admin.component'
-import { LoginFormComponent } from './pages/login-form/login-form.component'
-import { NewPollComponent } from './pages/new-poll/new-poll.component'
-import { UpdateFormComponent } from './pages/update-form/update-form.component';
-import { ConfirmComponent } from './core/misc/confirm.component';
-import { UserInfoComponent } from './pages/user-info/user-info.component';
-import { PollGridComponent } from './pages/poll-grid/poll-grid.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    ProfileComponent,
-    PollComponent,
-    AdminComponent,
-    LoadingComponent,
-    LoginFormComponent,
-    NewPollComponent,
-    UpdateFormComponent,
-    ConfirmComponent,
-    UserInfoComponent,
-    PollGridComponent,
-    SearchFilterPipe
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -59,16 +28,7 @@ import { PollGridComponent } from './pages/poll-grid/poll-grid.component'
     NgbModule.forRoot()
   ],
   providers: [
-    AuthService,
-    ApiService,
-    LoginFormService,
-    ConfirmService,
-    ChartService
-  ],
-  entryComponents: [
-    LoginFormComponent,
-    ConfirmComponent,
-    NewPollComponent
+    ApiService
   ],
   bootstrap: [AppComponent]
 })
